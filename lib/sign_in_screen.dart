003c9f2f575ead837d337b3c55a64e51138276ee
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-05-14 21:21:51 +0900

    メールアドレス・パスワードを使った新規登録の処理を追加

--- lib/sign_in_screen.dart
+++ lib/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:photoapp/photo_list_screen.dart';
 
@@ -10,6 +11,11 @@ class SignInScreen extends StatefulWidget {
 
 class _SignInScreenState extends State<SignInScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // メールアドレス用のTextEditingController
+  final TextEditingController _emailController = TextEditingController();
+  // パスワード用のTextEditingController
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +34,7 @@ class _SignInScreenState extends State<SignInScreen> {
                 ),
                 const SizedBox(height: 16),
                 TextFormField(
+                  controller: _emailController,
                   decoration: const InputDecoration(
                     labelText: 'メールアドレス',
                   ),
@@ -41,6 +48,7 @@ class _SignInScreenState extends State<SignInScreen> {
                 ),
                 const SizedBox(height: 8),
                 TextFormField(
+                  controller: _passwordController,
                   decoration: const InputDecoration(
                     labelText: 'パスワード',
                   ),
@@ -89,15 +97,33 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
-  void _onSignUp() {
-    if (_formKey.currentState?.validate() != true) {
-      return;
-    }
+  // 新規登録
+  Future<void> _onSignUp() async {
+    try {
+      if (_formKey.currentState?.validate() != true) {
+        return;
+      }
 
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (_) => const PhotoListScreen(),
-      ),
-    );
+      final String email = _emailController.text;
+      final String password = _passwordController.text;
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => const PhotoListScreen(),
+        ),
+      );
+    } catch (e) {
+      await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('エラー'),
+            content: Text(e.toString()),
+          );
+        },
+      );
+    }
   }
 }
